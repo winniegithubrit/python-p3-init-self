fd06783b7c0df2870de5c55cee8948d4264466ab
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-05-18 12:54:34 +0300

    Completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self,name,breed="Mutt"):
+       self.name = name
+       self.breed = breed
+
+
+    # def __init__(self,breed):
+    #     self.breed = breed
+    
+
+    # pass
